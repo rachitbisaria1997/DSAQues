493a894d627b52b4355fef6d3fb14e310509a94f
rachit <rachit.bisaria@gmail.com>
2025-10-31 11:28:28 +0530

    added files

---
+++ src/RunnableCallable/ForkJoinPool.java
@@ -0,0 +1,15 @@
+package RunnableCallable;
+
+// here a bigger task is broken into smaller subtasks, it is designed to achieve parallelism.
+// Instead of one thread doing all the work, ForkJoinPool:
+// Splits the array into smaller chunks (fork)
+// Processes those chunks in parallel on multiple threads
+//Combines (joins) the results back together
+
+public class ForkJoinPool {
+
+    public static void main(String[] args){
+
+    }
+
+}
--- src/two_pointers/Gfg.java
+++ src/two_pointers/Gfg.java
@@ -1,5 +1,9 @@
 package two_pointers;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Gfg {
 
     static int removeElement(int[] arr, int el){
@@ -15,7 +19,28 @@ public class Gfg {
         return slow;
 
     }
+    
+    
+    static void removeElem(int[] arr, int ele){
+        int count = 0;
 
+        for(int num : arr){
+            if(num != ele){
+                count++;
+            }
+        }
+
+        int[] newArr = new int[count];
+        int j = 0;
+        for(int x : arr){
+            if(x != ele){
+                newArr[j++] = x;
+            }
+        }
+
+        System.out.println(Arrays.toString(newArr));
+    }
+    
     public static void main(String[] args){
         int[] arr = {0, 1, 3, 0, 2, 2, 4, 2};
         int targrt = 2;
